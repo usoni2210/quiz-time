cc349fde5139f0f69bea90c2662a2a0a57e549f0
Umesh Soni <usoni2210@gmail.com>
2020-05-02 03:56:13 +0530

    update images url

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ if [[ "$1" == "--build-images" ]]; then
   TAG="v1.0-${TIMESTAMP}"
 
   echo "Building Quiz-Time web-app image"
-  docker build app -t "twister22/quiz-time/web-app:$TAG" >/dev/null
+  docker build app -t "twister22/quiz-time.web-app:$TAG" >/dev/null
 
   echo "Building Quiz-Time init-db image"
-  docker build database -t "twister22/quiz-time/init-db:$TAG" >/dev/null
+  docker build database -t "twister22/quiz-time.init-db:$TAG" >/dev/null
 
   echo "Installing Helm Chart"
   helm install quiz-time helm-chart --set init_db.image.tag="$TAG",quiz_time.image.tag="$TAG"
